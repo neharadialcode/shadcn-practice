36bca162b7f713b44e3033dcf6c2670c6cd0c7e9
anilkumar70820 <ak9565191@gmail.com>
2024-03-04 12:52:01 +0530

    make roaster tab

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import TabsComponents from "@/components/TabsComponents";
+import RoasterTab from "@/components/RoasterTab";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <TabsComponents />;
+  return (
+  <>
+   <TabsComponents />
+   <RoasterTab/>
+   </>
+  );
 }
