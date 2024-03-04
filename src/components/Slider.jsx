f49d5542949cf30eed8a243f83cb937a17018815
Sandeep-Luhaniwal <sandeepluh0001.com>
2024-03-04 12:52:17 +0530

    slider components create and card add in slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+
+export function Slider() {
+    return (
+        <div className=" ">
+            <h2 className="text-center capitalize font-medium px-4 pb-12 text-3xl sm:text-4xl md:text-[40px]">Best <span className="text-[#d3756b] font-extrabold">roasters</span> in the town</h2>
+            <Carousel className="w-full max-w-[1140px] mx-auto">
+                <CarouselContent>
+                    {Array.from({ length: 4 }).map((_, index) => (
+                        <CarouselItem key={index} className="">
+                            <Card className="max-w-[357px] p-0 bg-white">
+                                <CardContent className="flex flex-col">
+                                    <Image width={357} className="rounded-2xl" height={280} src="/webp/slider_image_first.webp" alt="image slider" />
+                                    <h3 className="font-semibold text-xl pt-4">Coffee Bean by Malerapaso</h3>
+                                    <p className="font-normal text-sm pt-2">Plantilla de informe de resonancia magnética (RM) de articulación de Plantilla de informe de resonancia magnética (RM) de articulación de Plantilla de informe de resonancia magnética (RM) de articulación de</p>
+                                    <button className="py-4 border-opacity-15 border rounded-3xl mt-4 text-base font-normal">More</button>
+                                </CardContent>
+                            </Card>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
+    )
+}
+
+
+export default Slider
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import TabsComponents from "@/components/TabsComponents";
+import Slider from "@/components/Slider";
+// import TabsComponents from "@/components/TabsComponents";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <TabsComponents />;
+  return <Slider />;
 }
