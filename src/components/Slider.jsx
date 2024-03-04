2dcc4bc286825d356cff812c9b161cc0f27fb4b5
Sandeep-Luhaniwal <sandeepluh0001.com>
2024-03-04 15:21:47 +0530

    slider button hover add

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -13,7 +13,7 @@ import { sliderCard } from "./Helper"
 
 export function Slider() {
     return (
-        <div className=" overflow-hidden">
+        <div className=" overflow-hidden py-12 sm:py-16 md:py-20">
             <div className="w-full px-3 xl:px-0 max-w-[1140px] mx-auto">
                 <h2 className="text-center capitalize font-medium px-4 pb-12 text-3xl sm:text-4xl md:text-[40px]">Best <span className="text-[#d3756b] font-extrabold">roasters</span> in the town</h2>
                 <Carousel className="pt-0 w-full max-auto">
@@ -25,7 +25,7 @@ export function Slider() {
                                         <Image width={357} className="rounded-2xl w-full max-h-[280px]" height={280} src={value.image} alt="image slider" />
                                         <h3 className="font-semibold text-xl pt-4">{value.title}</h3>
                                         <p className="font-normal text-sm pt-2">{value.description}</p>
-                                        <button className="py-4 border-opacity-15 border rounded-3xl mt-4 text-base font-normal">More</button>
+                                        <button className="py-4 hover:bg-black hover:text-white duration-300 border-opacity-15 border rounded-3xl mt-4 text-base font-normal">More</button>
                                     </CardContent>
                                 </Card>
                             </CarouselItem>
