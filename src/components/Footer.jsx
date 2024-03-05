b08fcff95a0e9a71df000618718c368c41aec7e1
anilkumar70820 <ak9565191@gmail.com>
2024-03-05 09:52:56 +0530

    remove unused images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ let currentYear = new Date().getFullYear();
 const Footer = () => {
   return (
     <div className="mx-3">
-      <div className="max-w-[1360px] mx-auto bg-[url('/assets/footer-bg.png')] bg-cover xl:bg-[length:100%_100%] bg-center bg-no-repeat rounded-[32px_32px_0px_0px]">
+      <div className="max-w-[1360px] mx-auto bg-[url('/assets/images/png/footer_bg.png')] bg-cover xl:bg-[length:100%_100%] bg-center bg-no-repeat rounded-[32px_32px_0px_0px]">
         <div className="max-w-[1232px] mx-auto md:pb-[55px] md:pt-12 py-12 xl:px-0 px-3">
           <div className="flex justify-between flex-wrap gap-4">
             <div className="w-full md:w-5/12">
@@ -17,7 +17,7 @@ const Footer = () => {
                     width={203}
                     height={56}
                     className="w-[160px] lg:w-[204px]"
-                    src="/footer-logo.svg"
+                    src="/assets/images/svg/footer_logo.svg"
                     alt="logo"
                   />
                 </Link>
