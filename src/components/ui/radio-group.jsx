14731c79b91e7461c4b4bb1f4e2f76edc611bc66
anilkumar70820 <ak9565191@gmail.com>
2024-03-06 12:22:33 +0530

    add radio button

---
+++ src/components/ui/radio-group.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+import { Circle } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { InputRadio } from "../Icons";
+
+const RadioGroup = React.forwardRef(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn("grid gap-2", className)}
+      {...props}
+      ref={ref}
+    />
+  );
+});
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+
+const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "w-4 h-4 rounded-full border-2 border-[#e6e6e6] flex justify-center items-center",
+        className
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className="h-4 w-4 ">
+        <InputRadio className="w-4 h-4" />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  );
+});
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
+
+export { RadioGroup, RadioGroupItem };
