299875231a5d0922cd6b47e379ab121f4525a2c6
Sandeep-Luhaniwal <sandeepluh0001.com>
2024-03-06 12:16:08 +0530

    some mistake slove in slider

--- src/components/ui/carousel.jsx
+++ src/components/ui/carousel.jsx
@@ -157,7 +157,7 @@ const CarouselPrevious = React.forwardRef(({ className, variant = "outline", siz
       ref={ref}
       variant={variant}
       size={size}
-      className={cn("absolute border-transparent rounded-full  hover:opacity-100 xl:opacity-100 xl:hover:bg-white opacity-80 h-8 w-8 md:h-10 md:w-10 px-[14px] xl:px-0 bg-white", orientation === "horizontal"
+      className={cn("absolute border-transparent rounded-full  hover:opacity-100 xl:opacity-100 xl:hover:bg-white opacity-80 h-8 w-8 md:h-10 md:w-10 px-[11px] md:px-[14px] xl:px-0 bg-white", orientation === "horizontal"
         ? "left-1 xl:-left-16 min-[1300px]:-left-20 top-1/2 -translate-y-1/2"
         : "-top-0 left-1/2 -translate-x-1/2 rotate-90", className)}
       disabled={!canScrollPrev}
@@ -178,7 +178,7 @@ const CarouselNext = React.forwardRef(({ className, variant = "outline", size = 
       ref={ref}
       variant={variant}
       size={size}
-      className={cn("absolute border-transparent rounded-full  hover:opacity-100 xl:opacity-100 xl:hover:bg-white opacity-80 h-8 w-8 md:h-10 md:w-10 px-[14px] xl:px-0 bg-white", orientation === "horizontal"
+      className={cn("absolute border-transparent rounded-full  hover:opacity-100 xl:opacity-100 xl:hover:bg-white opacity-80 h-8 w-8 md:h-10 md:w-10 px-[11px] md:px-[14px] xl:px-0 bg-white", orientation === "horizontal"
         ? "right-1 xl:-right-16 min-[1300px]:-right-20 top-1/2 -translate-y-1/2"
         : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90", className)}
       disabled={!canScrollNext}
